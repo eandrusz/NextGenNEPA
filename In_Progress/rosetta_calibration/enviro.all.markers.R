74cb1a57625d97a34d0bb7de70e31c4fd8df4f10
Elizabeth Andruszkiewicz Allan <eaa326@gmail.com>
2022-04-14 14:46:41 -0700

    mimammal all environmental data

--- In_Progress/rosetta_calibration/enviro.all.markers.R
+++ In_Progress/rosetta_calibration/enviro.all.markers.R
@@ -17,41 +17,28 @@ all.enviro.hash.key <- readRDS(here("Output","classification_output", "MiFish.al
 
 enviro.all.reads <- sum(all.enviro.asv.table$nReads)
 # 15150742
-
 # annotated hashes sum of reads
 enviro.annotated.reads <- all.enviro.asv.table %>% 
   filter(Hash %in% all.enviro.hash.key$representative) 
 enviro.annotated.reads <- sum(enviro.annotated.reads$nReads)
 # 11925664
-
 percentannotated <- 11925664/15150742*100
 # 78% not bad
 
-annotated.asv.table <- all.enviro.asv.table %>% 
+simple.taxonomy <- all.enviro.hash.key %>% 
+  select(c(representative,species))
+
+tax.table.to.write <- all.enviro.asv.table %>% 
   rename(representative = Hash) %>% 
-  left_join(all.enviro.hash.key, by = "representative")
-
-
-
-#### write hash key and dada2 output for only salmonids with only hashes found in mock community 
-taxonomy.to.write <- enviro.salmon.asv.table %>% 
-  filter(representative %in% salmon.hashes) %>% 
-  dplyr::select(c(representative, taxon)) %>% 
-  distinct()
-
-asv.table.to.write <- all.enviro.asv.table %>% 
-  filter(Hash %in% salmon.hashes) 
-
-tax.table.to.write <- asv.table.to.write %>% 
-  dplyr::rename(representative = Hash) %>% 
-  left_join(taxonomy.to.write, by = "representative") %>% 
+  left_join(simple.taxonomy, by = "representative") %>% 
   select(-Locus) %>% 
-  group_by(Sample_name, taxon) %>% 
+  group_by(Sample_name, species) %>% 
   summarize(Nreads = sum(nReads)) %>% 
-  filter(!is.na(taxon)) %>% 
-  rename(species = taxon) %>% 
+  filter(!is.na(species)) %>% 
+  rename(species = species) %>% 
   filter(!str_detect(Sample_name, "Kangaroo")) %>% 
-  mutate(., Sample_name = case_when(Sample_name == "MiFish.0321.1Prt.Dn.1TR3" ~ "MiFish.0321.1Prt.Dn.1.TR3",
+  mutate(., Sample_name = case_when(Sample_name == "MiFish.0321.1Prt.Dn.1TR2" ~ "MiFish.0321.1Prt.Dn.1.TR2",
+                             Sample_name == "MiFish.0321.1Prt.Dn.1TR3" ~ "MiFish.0321.1Prt.Dn.1.TR3",
                              Sample_name == "MiFish.0421.3Chk.Dn.3TR2" ~ "MiFish.0421.3Chk.Dn.3.TR2",
                              Sample_name == "MiFish.0421.3Chk.Dn.3TR3" ~ "MiFish.0421.3Chk.Dn.3.TR3",
                              Sample_name == "MiFish.0821.2Brn.Dn.2TR2" ~ "MiFish.0821.2Brn.Dn.2.TR2", 
@@ -64,7 +51,7 @@ tax.table.to.write <- asv.table.to.write %>%
                              is.na(tech) ~ 1)) 
   
 
-write_rds(tax.table.to.write, file=paste0(here("In_Progress","rosetta_calibration","data"),"/MiFish.salmonidonly.envirodata.RDS"))
+write_rds(tax.table.to.write, file=paste0(here("In_Progress","rosetta_calibration","data"),"/MiFish.all.envirodata.RDS"))
 
 
 tax.table.to.write %>% 
